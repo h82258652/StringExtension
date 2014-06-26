371d3b71385c1297248db504fc96dcaf28126c36
h82258652 <842053625@qq.com>
2014-06-26 14:05:02 +0800

    update

---
+++ StringExtension.Net45/StringExtension/StringExtension.IsInt32.cs
@@ -0,0 +1,17 @@
+﻿
+namespace System
+{
+    public static partial class StringExtension
+    {
+        /// <summary>
+        /// 指示当前字符串是否能转换为 32 位有符号整数。
+        /// </summary>
+        /// <param name="value">当前 System.String 对象。</param>
+        /// <returns>是否能转换为 32 位有符号整数。</returns>
+        public static bool IsInt32(this string value)
+        {
+            int i;
+            return int.TryParse(value, out i);
+        }
+    }
+}
