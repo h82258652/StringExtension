625c10a2b3a97123d4a6175429dfee3e283d2a3a
h82258652 <842053625@qq.com>
2014-06-27 08:44:54 +0800

    update
    add isint64

---
+++ StringExtension.Net45/StringExtension/StringExtension.IsInt64.cs
@@ -0,0 +1,17 @@
+﻿
+namespace System
+{
+    public static partial class StringExtension
+    {
+        /// <summary>
+        /// 指示当前字符串是否能转换为 64 位有符号整数。
+        /// </summary>
+        /// <param name="value">当前 System.String 对象。</param>
+        /// <returns>是否能转换为 64 位有符号整数。</returns>
+        public static bool IsInt64(this string value)
+        {
+            long l;
+            return long.TryParse(value, out l);
+        }
+    }
+}
