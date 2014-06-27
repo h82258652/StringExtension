0e1b96ea11310fafedabd0799be5638ade381d9b
h82258652 <842053625@qq.com>
2014-06-27 11:02:18 +0800

    update
    add isuint16

---
+++ StringExtension.Net45/StringExtension/StringExtension.IsUInt16.cs
@@ -0,0 +1,17 @@
+﻿
+namespace System
+{
+    public static partial class StringExtension
+    {
+        /// <summary>
+        /// 指示当前字符串是否能转换为 16 位无符号整数。
+        /// </summary>
+        /// <param name="value">当前 System.String 对象。</param>
+        /// <returns>是否能转换为 16 位无符号整数。</returns>
+        public static bool IsUInt16(this string value)
+        {
+            ushort us;
+            return ushort.TryParse(value, out us);
+        }
+    }
+}
