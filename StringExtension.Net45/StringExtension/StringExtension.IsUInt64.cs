117c890fd1f2f5c7ca74fb2580ed227b51719f19
h82258652 <842053625@qq.com>
2014-06-27 11:14:37 +0800

    update
    add isulong

---
+++ StringExtension.Net45/StringExtension/StringExtension.IsUInt64.cs
@@ -0,0 +1,17 @@
+﻿
+namespace System
+{
+    public static partial class StringExtension
+    {
+        /// <summary>
+        /// 指示当前字符串是否能转换为 64 位无符号整数。
+        /// </summary>
+        /// <param name="value">当前 System.String 对象。</param>
+        /// <returns>是否能转换为 64 位无符号整数。</returns>
+        public static bool IsUInt64(this string value)
+        {
+            ulong ul;
+            return ulong.TryParse(value, out ul);
+        }
+    }
+}
