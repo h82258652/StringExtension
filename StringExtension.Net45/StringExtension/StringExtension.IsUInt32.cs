6a30264d2ac9f7c12f14322a0491580ca08d6b29
h82258652 <842053625@qq.com>
2014-06-27 11:13:05 +0800

    update
    add isuint32

---
+++ StringExtension.Net45/StringExtension/StringExtension.IsUInt32.cs
@@ -0,0 +1,17 @@
+﻿
+namespace System
+{
+    public static partial class StringExtension
+    {
+        /// <summary>
+        /// 指示当前字符串是否能转换为 32 位无符号整数。
+        /// </summary>
+        /// <param name="value">当前 System.String 对象。</param>
+        /// <returns>是否能转换为 32 位无符号整数。</returns>
+        public static bool IsUInt32(this string value)
+        {
+            uint ui;
+            return uint.TryParse(value, out ui);
+        }
+    }
+}
