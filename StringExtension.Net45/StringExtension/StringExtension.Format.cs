4bec61c4a3a1a410427f093d69e570cca70ed3cf
h82258652 <842053625@qq.com>
2014-06-27 12:47:52 +0800

    update
    add a new format method

--- StringExtension.Net45/StringExtension/StringExtension.Format.cs
+++ StringExtension.Net45/StringExtension/StringExtension.Format.cs
@@ -1,4 +1,5 @@
-﻿
+﻿using System.Diagnostics.CodeAnalysis;
+
 namespace System
 {
     public static partial class StringExtension
@@ -11,9 +12,26 @@ namespace System
         /// <returns>format 的一个副本，其中格式项已替换为 args 中相应对象的字符串表示形式。</returns>
         /// <exception cref="System.ArgumentNullException"><c>format</c> 或 <c>args</c> 为 null。</exception>
         /// <exception cref="System.FormatException"><c>format</c> 无效。 - 或 - 格式项的索引小于零或大于等于 <c>args</c> 数组的长度。</exception>
+        [SuppressMessage("Microsoft.Naming", "CA1719")]
+        [SuppressMessage("Microsoft.Globalization", "CA1305")]
         public static string Format(this string format, params object[] args)
         {
             return string.Format(format, args);
+        }
+
+        /// <summary>
+        /// 将指定字符串中的格式项替换为指定数组中相应对象的字符串表示形式。 指定的参数提供区域性特定的格式设置信息。
+        /// </summary>
+        /// <param name="format">符合格式字符串（参见“备注”）。</param>
+        /// <param name="provider">一个提供区域性特定的格式设置信息的对象。</param>
+        /// <param name="args">一个对象数组，其中包含零个或多个要设置格式的对象。</param>
+        /// <returns>format 的一个副本，其中格式项已替换为 args 中相应对象的字符串表示形式。</returns>
+        /// <exception cref="System.ArgumentNullException"><c>format</c> 或 <c>args</c> 为 null。</exception>
+        /// <exception cref="System.FormatException"><c>format</c> 无效。 - 或 - 格式项的索引小于零或大于等于 <c>args</c> 数组的长度。</exception>
+        [SuppressMessage("Microsoft.Naming", "CA1719")]
+        public static string Format(this string format, IFormatProvider provider, params object[] args)
+        {
+            return string.Format(provider, format, args);
         }
     }
 }
