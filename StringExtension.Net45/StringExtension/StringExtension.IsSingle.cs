6333f963bf0f277ba057d69698404f0126f891fb
h82258652 <842053625@qq.com>
2014-06-27 12:53:20 +0800

    update
    add issingle

---
+++ StringExtension.Net45/StringExtension/StringExtension.IsSingle.cs
@@ -0,0 +1,17 @@
+﻿
+namespace System
+{
+    public static partial class StringExtension
+    {
+        /// <summary>
+        /// 指示当前字符串是否能转换为单精度浮点数。
+        /// </summary>
+        /// <param name="value">当前 System.String 对象。</param>
+        /// <returns>是否能转换为单精度浮点数。</returns>
+        public static bool IsSingle(this string value)
+        {
+            float f;
+            return float.TryParse(value, out f);
+        }
+    }
+}
