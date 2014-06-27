c6fe9ed01d58d28ab84c13f5eaef1c3b982fdbd0
h82258652 <842053625@qq.com>
2014-06-27 10:16:30 +0800

    update
    add isEmail.

---
+++ StringExtension.Net45/StringExtension/StringExtension.IsEmail.cs
@@ -0,0 +1,27 @@
+﻿using System.Text.RegularExpressions;
+
+namespace System
+{
+    public static partial class StringExtension
+    {
+#if Net40
+        /// <summary>
+        /// 指示指定字符串是否为 E-mail 地址。
+        /// </summary>
+        /// <param name="value">当前 System.String 对象。</param>
+        /// <returns>若符合 E-mail 格式，则为 true，否则为 false。</returns>
+#endif
+#if Net45
+        /// <summary>
+        /// 指示指定字符串是否为 E-mail 地址。
+        /// </summary>
+        /// <param name="value">当前 System.String 对象。</param>
+        /// <returns>若符合 E-mail 格式，则为 true，否则为 false。</returns>
+        /// <exception cref="System.Text.RegularExpressions.RegexMatchTimeoutException">发生超时。</exception>
+#endif
+        public static bool IsEmail(this string value)
+        {
+            return value != null && Regex.IsMatch(value, @"^\S+@\S+\.\S+$");
+        }
+    }
+}
