5f5c1e74fa26692254e55af6dd07ea8728327eab
h82258652 <842053625@qq.com>
2014-06-27 08:47:06 +0800

    update
    add isint16.

---
+++ StringExtension.Net45/StringExtension/StringExtension.IsInt16.cs
@@ -0,0 +1,17 @@
+﻿
+namespace System
+{
+    public static partial class StringExtension
+    {
+        /// <summary>
+        /// 指示当前字符串是否能转换为 16 位有符号整数。
+        /// </summary>
+        /// <param name="value">测试的字符串。</param>
+        /// <returns>能否转换为 16 位有符号整数。</returns>
+        public static bool IsInt16(this string value)
+        {
+            short s;
+            return short.TryParse(value, out s);
+        }
+    }
+}
