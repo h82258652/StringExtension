6b4faad35d3e38da6ec447eeeed0b5bd1e3410d6
h82258652 <842053625@qq.com>
2014-07-01 13:20:55 +0800

    update
    add isboolean, isdecimal.

---
+++ StringExtension.Net45/StringExtension/StringExtension.IsBoolean.cs
@@ -0,0 +1,17 @@
+﻿
+namespace System
+{
+    public static partial class StringExtension
+    {
+        /// <summary>
+        /// 指示当前字符串是否能转换为布尔值。
+        /// </summary>
+        /// <param name="value">当前 System.String 对象。</param>
+        /// <returns>是否能转换为布尔值。</returns>
+        public static bool IsBoolean(this string value)
+        {
+            bool b;
+            return bool.TryParse(value, out b);
+        }
+    }
+}
---
+++ StringExtension.Net45/StringExtension/StringExtension.IsDecimal.cs
@@ -0,0 +1,17 @@
+﻿
+namespace System
+{
+    public partial class StringExtension
+    {
+        /// <summary>
+        /// 指示当前字符串是否能转换为 Decimal。
+        /// </summary>
+        /// <param name="value">当前 System.String 对象。</param>
+        /// <returns>是否能转换为 Decimal。</returns>
+        public static bool IsDecimal(this string value)
+        {
+            decimal d;
+            return decimal.TryParse(value, out d);
+        }
+    }
+}
