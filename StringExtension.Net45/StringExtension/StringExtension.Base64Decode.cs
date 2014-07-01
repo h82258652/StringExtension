d56e0bb7abe6005f8f2a71df0b176b6eab4610e8
h82258652 <842053625@qq.com>
2014-07-01 13:44:43 +0800

    update base64
    update base64

--- StringExtension.Net45/StringExtension/StringExtension.Base64Decode.cs
+++ StringExtension.Net45/StringExtension/StringExtension.Base64Decode.cs
@@ -12,8 +12,21 @@ namespace System
         /// <exception cref="System.ArgumentNullException"><c>value</c> 为 null。</exception>
         public static string Base64Decode(this string value)
         {
-            byte[] bytes = Convert.FromBase64String(value);
-            return Encoding.UTF8.GetString(bytes, 0, bytes.Length);
+            return value.Base64Decode(null);
+        }
+
+        /// <summary>
+        /// 将当前字符串进行 Base64 解码。
+        /// </summary>
+        /// <param name="value">当前 System.String 对象。</param>
+        /// <param name="encoding">字符编码方式。</param>
+        /// <returns>解码后的字符串。</returns>
+        /// <exception cref="System.ArgumentNullException"><c>value</c> 为 null。</exception>
+        public static string Base64Decode(this string value, Encoding encoding)
+        {
+            encoding = encoding ?? Encoding.UTF8;
+            var bytes = Convert.FromBase64String(value);
+            return encoding.GetString(bytes, 0, bytes.Length);
         }
 
         /// <summary>
@@ -23,7 +36,18 @@ namespace System
         /// <returns>解码后的字符串。</returns>
         public static string Base64DecodeSafely(this string value)
         {
-            return string.IsNullOrEmpty(value) ? value : Base64Decode(value);
+            return value == null ? null : value.Base64Decode();
+        }
+
+        /// <summary>
+        /// 将当前字符串进行 Base64 解码。
+        /// </summary>
+        /// <param name="value">当前 System.String 对象。</param>
+        /// <param name="encoding">字符编码方式。</param>
+        /// <returns>解码后的字符串。</returns>
+        public static string Base64DecodeSafely(this string value, Encoding encoding)
+        {
+            return value == null ? null : value.Base64Decode(encoding);
         }
     }
 }
--- StringExtension.Net45/StringExtension/StringExtension.Base64Encode.cs
+++ StringExtension.Net45/StringExtension/StringExtension.Base64Encode.cs
@@ -12,7 +12,20 @@ namespace System
         /// <exception cref="System.ArgumentNullException"><c>value</c> 为 null。</exception>
         public static string Base64Encode(this string value)
         {
-            var bytes = Encoding.UTF8.GetBytes(value);
+            return value.Base64Encode(null);
+        }
+
+        /// <summary>
+        /// 对当前字符串进行 Base64 编码。
+        /// </summary>
+        /// <param name="value">当前 System.String 对象。</param>
+        /// <param name="encoding">字符编码方式。</param>
+        /// <returns>编码后的字符串。</returns>
+        /// <exception cref="System.ArgumentNullException"><c>value</c> 为 null。</exception>
+        public static string Base64Encode(this string value, Encoding encoding)
+        {
+            encoding = encoding ?? Encoding.UTF8;
+            var bytes = encoding.GetBytes(value);
             return Convert.ToBase64String(bytes);
         }
 
@@ -23,7 +36,18 @@ namespace System
         /// <returns>编码后的字符串。</returns>
         public static string Base64EncodeSafely(this string value)
         {
-            return string.IsNullOrEmpty(value) ? value : Base64Encode(value);
+            return value == null ? null : value.Base64Encode();
+        }
+
+        /// <summary>
+        /// 将当前字符串进行 Base64 编码。
+        /// </summary>
+        /// <param name="value">当前 System.String 对象。</param>
+        /// <param name="encoding">字符编码方式。</param>
+        /// <returns>编码后的字符串。</returns>
+        public static string Base64EncodeSafely(this string value, Encoding encoding)
+        {
+            return value == null ? null : value.Base64Encode(encoding);
         }
     }
 }
