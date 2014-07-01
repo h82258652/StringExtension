415c3c3b697aa918b93fc1cd064b413455b26475
h82258652 <842053625@qq.com>
2014-07-01 13:57:04 +0800

    add isenum
    rt

---
+++ StringExtension.Net45/StringExtension/StringExtension.IsEnum.cs
@@ -0,0 +1,39 @@
+﻿
+namespace System
+{
+    public static partial class StringExtension
+    {
+        /// <summary>
+        /// 指示当前字符串是否能转换为枚举值。
+        /// </summary>
+        /// <typeparam name="T">枚举类型。</typeparam>
+        /// <param name="value">当前 System.String 对象。</param>
+        /// <returns>能否转换为枚举值。</returns>
+        /// <exception cref="System.ArgumentException"><c>T</c> 不是枚举类型。</exception>
+        public static bool IsEnum<T>(this string value) where T : struct, IComparable, IFormattable
+#if !Portable
+, IConvertible
+#endif
+        {
+            T result;
+            return Enum.TryParse(value, out result);
+        }
+
+        /// <summary>
+        /// 指示当前字符串是否能转换为枚举值。
+        /// </summary>
+        /// <typeparam name="T">枚举类型。</typeparam>
+        /// <param name="value">当前 System.String 对象。</param>
+        /// <param name="ignoreCase">是否区分大小写。</param>
+        /// <returns>能否转换为枚举值。</returns>
+        /// <exception cref="System.ArgumentException"><c>T</c> 不是枚举类型。</exception>
+        public static bool IsEnum<T>(this string value, bool ignoreCase) where T : struct ,IComparable, IFormattable
+#if !Portable
+, IConvertible
+#endif
+        {
+            T result;
+            return Enum.TryParse(value, ignoreCase, out result);
+        }
+    }
+}
